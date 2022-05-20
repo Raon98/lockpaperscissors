7bc0c879139aa93f9bc0c394a83079fe79c2a68d
Raon <cheol1998@kakao.com>
2022-05-20 16:45:35 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="computer" :style="computeStyleObject"></div>
+    <div>
+      <button :style="{background: coaa}" @onClick="onClickButton()">바위</button>
+      <button @onClick="onClickButton('가위')">가위</button>
+      <button @onClick="onClickButton('보')">보</button>
+    </div>
+    <div>{{result}}</div>
+    <div>현재 {{score}}점</div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const rspCoords = {
+  바위 : '0',
+  가위 : "-142px",
+  보 : "-284px",
+}
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data(){
+    return{
+      coaa : '',
+      imgCoord: rspCoords.바위,
+      result: '',
+      score: 0,
+    }
+  },
+  computed: {
+    computeStyleObject() {
+      return{
+        background: `url(http://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgCoord} 0`,
+      };
+    },
+  },
+  methods: {
+    // eslint-disable-next-line no-unused-vars
+    onClickButton() {
+      this.coaa = 'red'
+    },
+    onclick: function (){
+      this.coaa = 'red'
+    }
+  },
+
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#computer {
+  width: 142px;
+  height: 200px;
+  background-position: 0 0;
 }
 </style>
